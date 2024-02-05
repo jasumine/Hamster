6247134b14f416751b120381f09d8ccd37cec662
지원 <92582758+jasumine@users.noreply.github.com>
2024-02-05 17:47:10 +0900

    .

--- Assets/Script/AdsManager.cs
+++ Assets/Script/AdsManager.cs
@@ -7,99 +7,195 @@ using System;
 
 public class AdsManager : MonoBehaviour
 {
-    private BannerView bannerView;
+    //    private BannerView bannerView;
+
+    //    public void Start()
+    //    {
+    //        MobileAds.Initialize((InitializationStatus initStatus) => { });
+    //    }
+    //#if UNITY_ANDROID
+    //    string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+
+    //#elif UNITY_IPHON
+    //     string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+    //#else
+    //       string adUnitId = "unexpected_platform";
+
+    //#endif
+
+
+    //    public void CreateBannerView()
+    //    {
+    //        Debug.Log("Creating banner view");
+
+    //        if (bannerView != null)
+    //        {
+    //            DestroyBannerView();
+    //        }
+
+    //        bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.BottomLeft);
+
+    //        //AdSize adSize = new AdSize(0, 0);
+    //        //bannerView = new BannerView(adUnitId, adSize, AdPosition.Bottom);
+    //    }
+
+    //    public void LoadAd()
+    //    {
+    //        if(bannerView ==null)
+    //        {
+    //            CreateBannerView();
+    //        }
+
+    //        var adRequest = new AdRequest();
+
+    //        Debug.Log("Loading banner ad");
+    //        bannerView.LoadAd(adRequest);
+    //    }
+
+    //    private void ListenToAdEvents()
+    //    {
+    //        bannerView.OnBannerAdLoaded += () =>
+    //        {
+    //            Debug.Log("Banner view loaded an ad with response : " + bannerView.GetResponseInfo());
+    //        };
+
+    //        bannerView.OnBannerAdLoadFailed += (LoadAdError error) =>
+    //        {
+    //            Debug.LogError("Banner view failed to load an ad with error: " + error);
+    //        };
+
+    //        bannerView.OnAdPaid += (AdValue adValue) =>
+    //        {
+    //            Debug.Log(String.Format("Banner view paid {0} {1}.", adValue.Value, adValue.CurrencyCode));
+    //        };
+
+    //        bannerView.OnAdImpressionRecorded += () =>
+    //        {
+    //            Debug.Log("Banner view recorded an impression.");
+    //        };
+
+    //        bannerView.OnAdClicked += () =>
+    //        {
+    //            Debug.Log("Banner view was clicked.");
+    //        };
+
+    //        bannerView.OnAdFullScreenContentOpened += () =>
+    //        {
+    //            Debug.Log("Banner view full screen content opened.");
+    //        };
+
+    //        bannerView.OnAdFullScreenContentClosed += () =>
+    //        {
+    //            Debug.Log("Banner view full screen content closed.");
+    //        };
+    //    }
+
+    //    public void DestroyBannerView()
+    //    {
+    //        if(bannerView !=null)
+    //        {
+    //            Debug.Log("Destroying bannerview");
+    //            bannerView.Destroy();
+    //            bannerView = null;
+    //        }
+    //    }
+
+    string adUnitId;
+
+    BannerView _bannerView;
 
     public void Start()
     {
-        MobileAds.Initialize((InitializationStatus initStatus) => { });
-    }
-#if UNITY_ANDROID
-    string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+        MobileAds.Initialize((InitializationStatus initStatus) =>
+        {
+            //초기화 완료
+        });
 
-#elif UNITY_IPHON
-     string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#if UNITY_ANDROID
+        adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#elif UNITY_IOS
+            adUnitId = "ca-app-pub-3940256099942544/2934735716";
 #else
-       string adUnitId = "unexpected_platform";
-    
+            adUnitId = "unexpected_platform";
 #endif
 
-
-    public void CreateBannerView()
-    {
-        Debug.Log("Creating banner view");
-
-        if (bannerView != null)
-        {
-            DestroyBannerView();
-        }
-
-        bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.BottomLeft);
-
-        //AdSize adSize = new AdSize(0, 0);
-        //bannerView = new BannerView(adUnitId, adSize, AdPosition.Bottom);
+        LoadAd();
     }
 
-    public void LoadAd()
+    public void LoadAd() //광고 로드
     {
-        if(bannerView ==null)
+        if (_bannerView == null)
         {
             CreateBannerView();
         }
+        var adRequest = new AdRequest.Builder()
+            .AddKeyword("unity-admob-sample")
+            .Build();
 
-        var adRequest = new AdRequest();
-
-        Debug.Log("Loading banner ad");
-        bannerView.LoadAd(adRequest);
+        Debug.Log("Loading banner ad.");
+        _bannerView.LoadAd(adRequest);
     }
+
+    public void CreateBannerView() //광고 보여주기
+    {
+        Debug.Log("Creating banner view");
+
+        if (_bannerView != null)
+        {
+            DestroyAd();
+        }
+
+        _bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+
+        //_bannerView = new BannerView(_adUnitId, AdSize.Banner, 0, 50);
+    }
+
 
     private void ListenToAdEvents()
     {
-        bannerView.OnBannerAdLoaded += () =>
+        _bannerView.OnBannerAdLoaded += () =>
         {
-            Debug.Log("Banner view loaded an ad with response : " + bannerView.GetResponseInfo());
+            Debug.Log("Banner view loaded an ad with response : "
+                + _bannerView.GetResponseInfo());
         };
-
-        bannerView.OnBannerAdLoadFailed += (LoadAdError error) =>
+        _bannerView.OnBannerAdLoadFailed += (LoadAdError error) =>
         {
-            Debug.LogError("Banner view failed to load an ad with error: " + error);
+            Debug.LogError("Banner view failed to load an ad with error : "
+                + error);
         };
-
-        bannerView.OnAdPaid += (AdValue adValue) =>
+        _bannerView.OnAdPaid += (AdValue adValue) =>
         {
-            Debug.Log(String.Format("Banner view paid {0} {1}.", adValue.Value, adValue.CurrencyCode));
+            Debug.Log(string.Format("Banner view paid {0} {1}.",
+                adValue.Value,
+                adValue.CurrencyCode));
         };
-
-        bannerView.OnAdImpressionRecorded += () =>
+        _bannerView.OnAdImpressionRecorded += () =>
         {
             Debug.Log("Banner view recorded an impression.");
         };
-
-        bannerView.OnAdClicked += () =>
+        _bannerView.OnAdClicked += () =>
         {
             Debug.Log("Banner view was clicked.");
         };
-
-        bannerView.OnAdFullScreenContentOpened += () =>
+        _bannerView.OnAdFullScreenContentOpened += (null);
         {
             Debug.Log("Banner view full screen content opened.");
         };
-
-        bannerView.OnAdFullScreenContentClosed += () =>
+        _bannerView.OnAdFullScreenContentClosed += (null);
         {
             Debug.Log("Banner view full screen content closed.");
         };
     }
 
-    public void DestroyBannerView()
+    public void DestroyAd() //광고 제거
     {
-        if(bannerView !=null)
+        if (_bannerView != null)
         {
-            Debug.Log("Destroying bannerview");
-            bannerView.Destroy();
-            bannerView = null;
+            Debug.Log("Destroying banner ad.");
+            _bannerView.Destroy();
+            _bannerView = null;
         }
     }
-
 
 
 }
